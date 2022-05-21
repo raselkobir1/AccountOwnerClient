c13ec09e09e6fff56ee3e57745eefb21023108eb
raselkobir1 <raselkobir29@gmail.com>
2022-05-21 20:48:57 +0600

    create componet and repository for owner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import {CollapseModule } from 'ngx-bootstrap/collapse';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
