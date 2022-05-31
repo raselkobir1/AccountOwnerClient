849fe70ff40f0b6b34933a1ea1bee837433b3ddd
raselkobir1 <raselkobir29@gmail.com>
2022-06-01 00:00:07 +0600

    owner delete operation done

--- src/app/owner/owner-delete/owner-delete.component.ts
+++ src/app/owner/owner-delete/owner-delete.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandlerService } from 'src/app/shared/services/error-handler.service';
+import { OwnerRepositoryService } from 'src/app/shared/services/owner-repository.service';
+import { Owner } from 'src/app/_interfaces/owner.model';
+import { BsModalRef, ModalOptions, BsModalService } from 'ngx-bootstrap/modal';
+import { SuccessModalComponent } from 'src/app/shared/modals/success-modal/success-modal.component';
 
 @Component({
   selector: 'app-owner-delete',
@@ -6,10 +13,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-delete.component.css']
 })
 export class OwnerDeleteComponent implements OnInit {
-
-  constructor() { }
+  owner: Owner;
+  bsModalRef: BsModalRef;
+  constructor(private repository: OwnerRepositoryService, private errorHandler: ErrorHandlerService,
+    private router: Router, private activeRoute: ActivatedRoute, private modal: BsModalService) { }
 
   ngOnInit(): void {
+    this.getOwnerById();
   }
 
+  private getOwnerById = () => {
+    const ownerId: string = this.activeRoute.snapshot.params['id'];
+    const apiUri: string = `api/owner/${ownerId}`;
+    this.repository.getOwner(apiUri)
+      .subscribe({
+        next: (own: Owner) => this.owner = own,
+        error: (err: HttpErrorResponse) => this.errorHandler.handleError(err)
+      })
+  }
+
+  redirectToOwnerList = () => {
+    this.router.navigate(['/owner/list']);
+  }
+  
+  deleteOwner = () => {
+    const deleteUri: string = `api/owner/${this.owner.id}`;
+
+    this.repository.deleteOwner(deleteUri)
+      .subscribe({
+        next: (_) => {
+          const config: ModalOptions = {
+            initialState: {
+              modalHeaderText: 'Owner deleted Successfully',
+              okButtonText: 'Ok'
+            }
+          };
+          this.bsModalRef = this.modal.show(SuccessModalComponent, config);
+          this.bsModalRef.content.redirectOnOk.subscribe(_ => this.redirectToOwnerList());
+        },
+        error: (err: HttpErrorResponse) => this.errorHandler.handleError(err)
+      })
+  }
 }
