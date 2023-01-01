8940ac3d26f95c3d68f715e19811849aaca43e50
raselkobir1 <raselkobir29@gmail.com>
2023-01-01 23:37:27 +0600

    test commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
 
 /*
   Here we import OnInit interface which defines the ngOnInit function. This function will execute any logic inside it as soon as the component initializes.
-
+  test change
   We can notice the constructor as well. The constructor is intended only for the injection of the service into the component. For any action that needs 
   to be executed upon component initialization, we should use the ngOnInit function.
 */
